9ef1527cca455dfe2d436d9d93524085dc0c8143
Saksham <khrn.saksham001@gmail.com>
2016-06-13 15:24:04 +0530

    DatabaseFetchActivity Created/Completed + Database Initialized with Individual Details

--- app/src/main/java/com/example/dawn/caloriecal/database/DatabaseFetchActivity.java
+++ app/src/main/java/com/example/dawn/caloriecal/database/DatabaseFetchActivity.java
@@ -1,9 +1,49 @@
 package com.example.dawn.caloriecal.database;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Dawn on 6/13/2016.
  */
-public class DatabaseFetchActivity
+public class DatabaseFetchActivity extends AppCompatActivity
 {
+    public Cursor getCuisine(SQLiteDatabase sqLiteDatabase)
+    {
+        Cursor cursor = sqLiteDatabase.rawQuery("select cuisine_name FROM cuisine;",null);
+        return cursor;
+    }
+
+    public Cursor getBreakfastItems(SQLiteDatabase sqLiteDatabase,String meal_time)
+    {
+        Cursor cursor = sqLiteDatabase.rawQuery("select name,cuisine,calorie from meals WHERE meal_time='"+meal_time+"';",null);
+        return cursor;
+    }
+
+    public int getConsumedCalories(SQLiteDatabase sqLiteDatabase)
+    {
+        int calories;
+        Cursor cursor = sqLiteDatabase.rawQuery("select sum(calories) as total_calories FROM meals;",null);
+        calories = cursor.getInt(cursor.getColumnIndex("total_calories"));
+        return calories;
+    }
+
+    public String getUserName(SQLiteDatabase sqLiteDatabase)
+    {
+        String userName;
+        Cursor cursor = sqLiteDatabase.rawQuery("select name from individual;",null);
+        userName = cursor.getString(cursor.getColumnIndex("name"));
+        return userName;
+    }
+
+    public int getUserTotalCalories(SQLiteDatabase sqLiteDatabase)
+    {
+        int calories;
+        Cursor cursor = sqLiteDatabase.rawQuery("Select calories from individual;",null);
+        calories=cursor.getInt(cursor.getColumnIndex("calories"));
+        return calories;
+    }
+
 
 }
--- app/src/main/java/com/example/dawn/caloriecal/database/DatabaseInitialize.java
+++ app/src/main/java/com/example/dawn/caloriecal/database/DatabaseInitialize.java
@@ -12,9 +12,10 @@ public class DatabaseInitialize extends AppCompatActivity {
 
     public void databaseCreation(SQLiteDatabase myDatabase) {
 
-        myDatabase.execSQL(CREATE+"Cuisine( _id INteger primary key autoincrement,cuisine_name varchar(20) not null");
+        myDatabase.execSQL(CREATE+"Cuisine( _id INteger primary key autoincrement,cuisine_name varchar(20) not null;");
         myDatabase.execSQL(CREATE + "Foodie( _id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR(30) NOT NULL,cuisine VARCHAR(20) not null,calorie INTEGER not null);");
         myDatabase.execSQL(CREATE + "Meals( _id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR(30) NOT NULL,cuisine VARCHAR(20) not null,calorie INTEGER not null);");
+        myDatabase.execSQL(CREATE + "Individual( _id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR(10) not null,calories INTEGER not null;");
     }
 
     public void cuisineNameInitialize(SQLiteDatabase myDatabase, String[] cuisine_name) {
@@ -26,9 +27,14 @@ public class DatabaseInitialize extends AppCompatActivity {
     public void foodInitialize(SQLiteDatabase myDatabase, String[] cuisine_name,String[] food_name,int[] calorie) {
 
         int size = cuisine_name.length,i;
-        for (i=0;i<size;i++) {
-
+        for (i=0;i<size;i++)
+        {
             myDatabase.execSQL(INSERT + "Foodie(name,cuisine,calorie) VALUES ('" + food_name[i] + "','"+cuisine_name+"','"+calorie[i]+"');");
         }
+    }
+
+    public void individualInitialize(SQLiteDatabase sqLiteDatabase,String name,int calories)
+    {
+        sqLiteDatabase.execSQL(INSERT +("Individual(name,calories) values ('"+name+"','"+calories+"');"));
     }
 }
