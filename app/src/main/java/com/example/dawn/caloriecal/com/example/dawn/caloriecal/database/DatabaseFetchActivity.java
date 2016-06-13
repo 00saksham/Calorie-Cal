2f6801e1592136d570b7bd33d8753d9e198cc239
Saksham <khrn.saksham001@gmail.com>
2016-06-13 13:14:00 +0530

    Database Initialized 13/6/16 13:13

---
+++ app/src/main/java/com/example/dawn/caloriecal/com/example/dawn/caloriecal/database/DatabaseFetchActivity.java
@@ -0,0 +1,9 @@
+package com.example.dawn.caloriecal.com.example.dawn.caloriecal.database;
+
+/**
+ * Created by Dawn on 6/13/2016.
+ */
+public class DatabaseFetchActivity
+{
+
+}
---
+++ app/src/main/java/com/example/dawn/caloriecal/com/example/dawn/caloriecal/database/DatabaseInitialize.java
@@ -0,0 +1,34 @@
+package com.example.dawn.caloriecal.com.example.dawn.caloriecal.database;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dawn on 6/13/2016.
+ */
+public class DatabaseInitialize extends AppCompatActivity {
+    private static String INSERT = "INSERT INTO ";
+    private static String CREATE = "CREATE TABLE IF NOT EXISTS ";
+
+    public void databaseCreation(SQLiteDatabase myDatabase) {
+
+        myDatabase.execSQL(CREATE+"Cuisine( _id INteger primary key autoincrement,cuisine_name varchar(20) not null");
+        myDatabase.execSQL(CREATE + "Foodie( _id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR(30) NOT NULL,cuisine VARCHAR(20) not null,calorie INTEGER not null);");
+        myDatabase.execSQL(CREATE + "Meals( _id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR(30) NOT NULL,cuisine VARCHAR(20) not null,calorie INTEGER not null);");
+    }
+
+    public void cuisineNameInitialize(SQLiteDatabase myDatabase, String[] cuisine_name) {
+        for (String item : cuisine_name) {
+            myDatabase.execSQL(INSERT + "Cuisine(cuisine_name) VALUES ('" + item + "');");
+        }
+    }
+
+    public void foodInitialize(SQLiteDatabase myDatabase, String[] cuisine_name,String[] food_name,int[] calorie) {
+
+        int size = cuisine_name.length,i;
+        for (i=0;i<size;i++) {
+
+            myDatabase.execSQL(INSERT + "Foodie(name,cuisine,calorie) VALUES ('" + food_name[i] + "','"+cuisine_name+"','"+calorie[i]+"');");
+        }
+    }
+}
